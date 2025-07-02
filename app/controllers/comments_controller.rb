a16f6030c2c1e088b27a134af3e50e470f2111e1
Ismaël Perez <ismael.jouhari@gmail.com>
2025-07-02 16:07:02 +0200

    [ADD] Added comments creation
    - Implemented CommentsController create method
    - Added user authentication and post association
    - Validation with flash messages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post
+  
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.post = @post
+    @comment.user = current_user
+    
+    if @comment.save
+      redirect_to @post, notice: 'Comment added successfully'
+    else
+      redirect_to @post, alert: 'Error adding comment.'
+    end
+  end
+  
+  private
+  
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+  
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
